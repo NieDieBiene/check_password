8c45684f141a086821f9dd1026e44563a0abb578
NieDieBiene <diebienepodp@gmail.com>
2026-05-31 16:22:03 +0300

    Add files via upload

---
+++ check_passwords.py
@@ -0,0 +1,52 @@
+def has_digit(password):
+    rate = 0
+    if any(c.isdigit() for c in password):
+        rate = 2
+    return rate
+
+
+def has_upper_letters(password):
+    rate = 0
+    if any(c.isupper() for c in password):
+        rate = 2
+    return rate
+
+
+def has_lower_letters(password):
+    rate = 0
+    if any(c.islower() for c in password):
+        rate = 2
+    return rate
+
+
+def is_very_long(password):
+    rate = 0
+    lenth = len(password)
+    if lenth > 12:
+        rate = 2
+    return rate
+
+
+def has_symbols(password):
+    rate = 0
+    if any(not c.isalnum() for c in password):
+        rate = 2
+    return rate
+
+
+cheks = [
+        has_digit,
+        is_very_long,
+        has_upper_letters,
+        has_lower_letters,
+        has_symbols
+    ]
+
+password = input('Введите пароль: ')
+password_rate = 0
+
+for func in cheks:
+    result = func(password)
+    password_rate += result
+
+print('Рейтинг пароля: ' + str(password_rate))
